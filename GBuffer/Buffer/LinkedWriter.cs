86479dc67a1ab88cde71af81bfe30e4b7143948b
blank100 <910438476@qq.com>
2023-06-12 15:01:01 +0800

    代码格式修改

---
+++ GBuffer/Buffer/LinkedWriter.cs
@@ -0,0 +1,12 @@
+﻿namespace Gal.Core
+{
+    /// <summary>
+    /// 链式写入缓冲器
+    /// </summary>
+    /// <para>author gouanlin</para>
+    /// <typeparam name="T"></typeparam>
+    public class LinkedWriter<T>
+    {
+        //TODO
+    }
+}
